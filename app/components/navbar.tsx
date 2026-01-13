0deea42dac172209a0f3c4d2263bc3aede1668f1
marinofranz <me@marino.codes>
2026-01-13 15:52:19 +0000

    bæta við verslun og produt card

---
+++ app/components/navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <nav className="container mx-auto px-4 py-4 max-w-5xl">
+      <div className="flex flex-row justify-between items-center">
+        <h1 className="text-xl font-bold">Aircraft Model Shop</h1>
+        <div className="flex space-x-4">
+          <Link href="/">Shop</Link>
+          <Link href="/cart">Cart</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
