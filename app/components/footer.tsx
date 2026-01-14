7ed67195b53f4fd943a2f4961e83b156ba0e4fb5
marinofranz <me@marino.codes>
2026-01-14 14:51:10 +0000

    bæta footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="w-full py-4 text-center text-sm mt-4 space-y-2">
+      <p className="text-gray-500">
+        &copy; {new Date().getFullYear()} Aircraft Model Shop
+      </p>
+    </footer>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
 
 const pjs = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={`${pjs.className} antialiased`}>
         <Navbar />
         <main className="container max-w-5xl mx-auto px-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
