d8d08f1dc473683b558cf3300e294adff8a138a1
marinofranz <me@marino.codes>
2026-01-16 15:01:11 +0000

    virkni á add to cart takka

--- app/components/product.tsx
+++ app/components/product.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import type { Product } from "../page";
+import { Button } from "./button";
+import { useState } from "react";
 
-// Ég bý bara til nýja týpu því ég vill fá annað nafn á props
 type ProductProps = Product;
 
 export function Product({ name, price, imageUrl }: ProductProps) {
+  const [amount, setAmount] = useState(0);
+
   return (
     <div className="border border-gray-300 rounded-lg overflow-hidden">
       <Image
@@ -19,14 +22,36 @@ export function Product({ name, price, imageUrl }: ProductProps) {
 
         <div className="flex flex-row gap-1 items-baseline bg-linear-to-r from-sky-50 to-blue-50 p-3 rounded-lg border border-sky-200">
           <span className="text-2xl font-bold text-sky-700">
-            {price.toLocaleString()}
+            {amount > 0
+              ? (price * amount).toLocaleString()
+              : price.toLocaleString()}
           </span>
           <span className="text-xs font-medium text-sky-600">ISK</span>
         </div>
 
-        <button className="w-full cursor-pointer bg-sky-500 text-white py-2 px-4 rounded hover:bg-sky-600 transition-colors mt-auto">
-          Add to Cart
-        </button>
+        {amount > 0 ? (
+          <div className="flex flex-row mt-auto">
+            <Button
+              className="rounded-r-none"
+              onClick={() => setAmount(amount - 1)}
+            >
+              -
+            </Button>
+            <Button className="w-full rounded-none" disabled>
+              {amount}
+            </Button>
+            <Button
+              className="rounded-l-none"
+              onClick={() => setAmount(amount + 1)}
+            >
+              +
+            </Button>
+          </div>
+        ) : (
+          <Button className="w-full" onClick={() => setAmount(amount + 1)}>
+            Add to Cart
+          </Button>
+        )}
       </div>
     </div>
   );
